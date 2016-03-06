1c8b25d0c3c67967ac4f39aed47f2add215aa25a
FelixLeal <felix@softwarecriollo.com>
2016-03-05 23:41:21 -0430

    Fix code

--- app/admin/buyer.rb
+++ app/admin/buyer.rb
@@ -26,7 +26,6 @@ ActiveAdmin.register Buyer do
       f.input :active
       f.input :password
       f.input :password_confirmation
-      #f.inputs "Buyer Info", for: :buyer_info do |t|
       f.inputs "Buyer Info", for: [:buyer_info, (f.object.buyer_info || BuyerInfo.new)] do |t|
         t.input :id, as: :hidden
         t.input :buyer_id, as: :hidden
@@ -34,21 +33,6 @@ ActiveAdmin.register Buyer do
         t.input :last_name
         t.input :phonenumber
       end
-      #f.has_one :buyer_info, heading: 'Buyer Info', allow_destroy: true do |c|
-      #  c.input :first_name
-      #  c.input :last_name
-      #  c.input :phonenumber
-      #end
-      #f.input :email, label: f.object.buyer_info
-      #f.input :password, label: "#{f.object.buyer_info.inspect}"
-=begin      
-      f.inputs 'Buyer Info', for: [:buyer_info, (f.object.buyer_info || f.object.build_buyer_info)],
-        id: 'buyer_id', class: 'inputs polyform' do |fc|
-        fc.input :first_name
-        fc.input :last_name
-        fc.input :phonenumber
-      end
-=end      
     end
     f.actions
   end
