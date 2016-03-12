7cb19f3ebfdd1694aee2ade83002cebd643f516f
felix <felix@softwarecriollo.com>
2016-03-12 18:52:30 -0430

    Configurar el envio de mensajes para produccion

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,18 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'mandados.herokuapp.com' }
+
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :domain               => "gmail.com",
+      :user_name            => 'felix@softwarecriollo.com',
+      :password             => 'felix18261871',
+      :authentication       => :login,
+      :enable_starttls_auto => true
+  }
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+
 end
